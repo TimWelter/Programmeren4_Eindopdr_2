a5407d6efcccee2893a21d32ae947109f425a4a9
Thomas Notenboom <tlganote@student.avans.nl>
2018-06-30 16:33:46 +0200

    Merge branch 'Category.Controller' into development

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -3,16 +3,16 @@ let routes = express.Router();
 let categoryController = require('../controllers/category.controller')
 
 //Getting category's
-routes.get('/category', categoryController.getAllCategories)
-routes.get('/category/:IDCategory', categoryController.getCategoryByID)
+routes.get('/category', categoryController.getAllCategories())
+routes.get('/category/:IDCategory', categoryController.getCategoryByID())
 
 //Posting a new category
-routes.post('/category', categoryController.addCategory)
+routes.post('/category', categoryController.addCategory())
 
 //Deleting a category
-routes.delete('/category/:IDCategory', categoryController.deleteCategoryByID)
+routes.delete('/category/:IDCategory', categoryController.deleteCategoryByID())
 
 //editing categories
-routes.put('/category/:IDCategory', categoryController.editCategoryByID)
+routes.put('/category/:IDCategory', categoryController.editCategoryByID())
 
 module.exports = routes
