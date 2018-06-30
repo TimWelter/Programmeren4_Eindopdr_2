f48dfccad773dcc582e198a9f536acee74ac90b2
Tim Welter <t.welter@avans.nl>
2018-06-30 17:31:01 +0200

    userID fixed

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -7,7 +7,7 @@ routes.get('/category', categoryController.getAllCategories)
 routes.get('/category/:IDCategory', categoryController.getCategoryByID)
 
 //Posting a new category
-routes.post('/category', categoryController.addCategor)
+routes.post('/category', categoryController.addCategory)
 
 //Deleting a category
 routes.delete('/category/:IDCategory', categoryController.deleteCategoryByID)
