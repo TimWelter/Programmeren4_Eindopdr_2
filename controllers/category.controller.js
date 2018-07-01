26ae2b4724f5e332a2d8e54accc4bf1dd3fbab41
Thomas Notenboom <tlganote@student.avans.nl>
2018-07-01 13:09:42 +0200

    changed category to Category

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,7 +1,7 @@
 const ApiError = require('../models/ApiError')
 const assert = require('assert')
 const db = require('../config/db')
-const category = require('../models/Category')
+let Category = require('../models/Category')
 
 
 module.exports = {
@@ -11,7 +11,7 @@ module.exports = {
         try {
             assert(req.user && req.user.id, 'User ID is missing!')
             assert(typeof (req.body) === 'object', 'request body must have an object containing naam and adres.')
-            category = new Category(req.body.naam, req.body.beschrijving)
+            Category = new Category(req.body.naam, req.body.beschrijving)
         } catch (ex) {
             const error = new ApiError(ex.toString(), 422)
             next(error)
@@ -19,7 +19,7 @@ module.exports = {
         }
 
         try {
-            db.query('INSERT INTO `categorie` (`Naam`, `Beschrijving`, UserID) VALUES (?,?,?)', [category.getName(), category.getDescription(), req.user.id],
+            db.query('INSERT INTO `categorie` (`Naam`, `Beschrijving`, UserID) VALUES (?,?,?)', [Category.getName(), Category.getDescription(), req.user.id],
                 (err, rows, fields) => {
                     if (err) {
                         const error = new ApiError(err.toString(), 412)
@@ -105,7 +105,7 @@ module.exports = {
             assert(req.user && req.user.id, 'User ID is missing!')
             assert(req.params.IDCategory, 'ID is missing!')
             assert(typeof (req.body) === 'object', 'request body must have an object containing naam and adres.')
-            category = new Category(req.body.naam, req.body.beschrijving)
+            Category = new Category(req.body.naam, req.body.beschrijving)
         } catch (ex) {
             const error = new ApiError(ex.toString(), 500)
             next(error)
@@ -113,10 +113,10 @@ module.exports = {
         }
         // Hier hebben we de juiste body als input.
 
-        const ID = req.params.IDCategory
+        let ID = req.params.IDCategory
         // 1. Zoek in db of studentenhuis met huisId bestaat
         try {
-            db.query('SELECT * FROM view_categorie WHERE ID = ?', [ID],
+            db.query('SELECT * FROM categorie WHERE ID = ?', [ID],
                 (err, rows, fields) => {
                     if (err) {
                         const error = new ApiError(err, 412)
@@ -137,7 +137,7 @@ module.exports = {
                             } else {
                                 //  - zo ja, dan SQL query UPDATE
                                 db.query(
-                                    'UPDATE categorie SET Naam = ?, Beschrijving = ? WHERE ID = ?', [category.getName(), category.getDescription(), req.params.IDCategory],
+                                    'UPDATE categorie SET Naam = ?, Beschrijving = ? WHERE ID = ?', [Category.getName(), Category.getDescription(), req.params.IDCategory],
                                     (err, rows, fields) => {
                                         if (err) {
                                             // handle error
@@ -174,12 +174,12 @@ module.exports = {
         }
         // Hier hebben we de juiste body als input.
 
-        const ID = req.params.IDCategory
+        let ID = req.params.IDCategory
         // 1. Zoek in db of studentenhuis met huisId bestaat
+        console.log(req.params.IDCategory)
         try {
-            db.query('SELECT * FROM view_categorie WHERE ID = ?', [ID],
+            db.query('SELECT * FROM `categorie` WHERE ID = ?', [ID],
                 (err, rows, fields) => {
-
                     if (err) {
                         const error = new ApiError(err, 412)
                         next(error);
@@ -199,7 +199,7 @@ module.exports = {
                             } else {
                                 //  - zo ja, dan SQL query UPDATE
                                 db.query(
-                                    'DELETE categorie WHERE ID = ?', [req.params.IDCategory],
+                                    'DELETE FROM categorie WHERE ID = ?', [req.params.IDCategory],
                                     (err, rows, fields) => {
                                         if (err) {
                                             // handle error
